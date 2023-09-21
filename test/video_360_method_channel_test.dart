bc8d3bee5073809be24294e0274d9ab752857094
kilroy80 <kilroy80@gmail.com>
2023-09-21 10:21:23 +0900

    feat: delete unused import

--- test/video_360_method_channel_test.dart
+++ test/video_360_method_channel_test.dart
@@ -1,27 +1,5 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:video_360/video_360_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  // MethodChannelVideo360 platform = MethodChannelVideo360();
-  // const MethodChannel channel = MethodChannel('video_360');
-  //
-  // setUp(() {
-  //   TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-  //     channel,
-  //     (MethodCall methodCall) async {
-  //       return '42';
-  //     },
-  //   );
-  // });
-  //
-  // tearDown(() {
-  //   TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  // });
-  //
-  // test('getPlatformVersion', () async {
-  //   expect(await platform.getPlatformVersion(), '42');
-  // });
 }
